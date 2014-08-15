e4dea73433238ce03e25005e92e668c7a3ccd305
Roy Lowrance <roy.lowrance@gmail.com>
2014-08-15 10:04:40 -0400

    move file from theis/src

---
+++ R/CompareModelsBmtpAssessor.R
@@ -0,0 +1,10 @@
+CompareModelsBmtpAssessor <- function(scenario, testing.period, data) {
+    # return list of Models and other objects for the scenario and testing period
+    # $Model : list of Models(data, training
+    # return a list of Models such that the model with the lowest RMSE in the testing.period 
+
+    # return best # training days for the scenario in the testing period
+    # use cross validation to select this
+    # OR
+    # return Model list, description list, Assess function (BETTER)
+}
