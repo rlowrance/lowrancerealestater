b9d211667d6e2104bc292c9602d2114a58ca5ded
Roy Lowrance <roy.lowrance@gmail.com>
2014-09-11 05:58:55 -0400

    no longer allow nrow arg

--- R/ReadSplit.R
+++ R/ReadSplit.R
@@ -1,7 +1,7 @@
-ReadSplit <- function(path.in, split.name, nrows, verbose) {
+ReadSplit <- function(path.in, split.name, verbose) {
     # read and return a one-column data frame from an rsave file
     #cat('starting Readplit', path.in, split.name, nrows, verbose, '\n'); browser()
-    path <- paste0(path.in, '-', split.name, '.rsave')
+    path <- paste0(path.in, split.name, '.RData')
     loaded <- load(file = path)
     stopifnot(loaded[[1]] == 'data')
     data
--- R/ReadTransactionSplits.R
+++ R/ReadTransactionSplits.R
@@ -1,27 +1,11 @@
-ReadTransactionSplits <- function(path.in.base, split.names, nrow = 0, verbose = FALSE) {
-    # return data.frame containing certain features for all transactions
-#    split.names <- c( 'saleDate'  # dates are used to select testing and training data
-#                     ,'recordingDate'
-#                     ,'price'
-#                     ,'log.price'
-#                     ,'apn'
-#                     ,Predictors('Chopra', form = 'log', center = TRUE, useAssessment = TRUE)
-#                     ,Predictors('Chopra', form = 'log', center = FALSE, useAssessment = TRUE)
-#                     ,Predictors('Chopra', form = 'level', center = TRUE, useAssessment = TRUE)
-#                     ,Predictors('Chopra', form = 'level', center = FALSE, useAssessment = TRUE)
-#                     ,Predictors('Chopra', form = 'log', center = TRUE, useAssessment = FALSE)
-#                     ,Predictors('Chopra', form = 'log', center = FALSE, useAssessment = FALSE)
-#                     ,Predictors('Chopra', form = 'level', center = TRUE, useAssessment = FALSE)
-#                     ,Predictors('Chopra', form = 'level', center = FALSE, useAssessment = FALSE)
-#                     )
-#    split.names.unique <- unique(split.names)
+ReadTransactionSplits <- function(path.in.base, split.names, verbose = FALSE) {
+    # return data.frame containing specified features for all transactions
 
     # append each split as a column onto a large data frame
     data <- NULL 
     for (split.name in split.names) {
         new.column <- ReadSplit( path.in = path.in.base
                                 ,split.name = split.name
-                                ,nrow = nrow
                                 ,verbose = verbose
                                 )
         if (verbose) cat('new.column', split.name, '\n')
